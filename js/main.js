6bcccb2477e2e7b25b1feab8c1d681a774f9efd2
RazorzMcLazorz <nateyoungdev@gmail.com>
2022-11-18 22:19:22 -0700

    made huge changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let isSunsetMode = true
+
 const languageWordsOuter = [
   'React.js',
   'JavaScript',
@@ -23,3 +25,25 @@ document.getElementById('spinInner').innerHTML = languageWordsInner.map((languag
     <div class="words">${language}</div>
   </div>`
 ).join('')
+
+document.getElementById("themeChange").addEventListener("click", function() {
+  toggleStylesheet("./css/theme.css", isSunsetMode);
+})
+
+function toggleStylesheet(href, onoff) {
+  console.log(onoff)
+  if (onoff) { 
+    var link = document.createElement('link');
+    link.id = 'theme';
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = href;
+    document.getElementsByTagName('head')[0].appendChild(link);
+    document.getElementById("sunsetMountains").src = './img/mSunrise.svg'
+    isSunsetMode = false
+  } else { 
+      document.getElementById("sunsetMountains").src = './img/mountainsSunset.svg'
+      document.querySelectorAll('link[id=theme]').forEach((link) => link.parentNode.removeChild(link))
+      isSunsetMode = true
+  }
+}
